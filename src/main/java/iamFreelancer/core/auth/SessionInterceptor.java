109c467bacb17bf114d8658c1ed4482b987d56ed
kykim <kykim@DESKTOP-ILHLP7L>
2021-07-14 01:31:48 +0900

    [세션]  - 세션 체크 하여 redirect하는 부분 수정

--- src/main/java/iamFreelancer/core/auth/SessionInterceptor.java
+++ src/main/java/iamFreelancer/core/auth/SessionInterceptor.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import iamFreelancer.login.util.LoginUserDetailHelper;
+import iamFreelancer.login.vo.UserVO;
 
 @Component
 public class SessionInterceptor implements HandlerInterceptor {
@@ -33,6 +34,20 @@ public class SessionInterceptor implements HandlerInterceptor {
         }
         */
         
+		UserVO userVO = loginUserDetailHelper.getAuthenticatedUser();
+		
+		// 세션이 없고 요청한 URL이 '/main'일 경우 요청 페이지로 이동
+		if ("[ROLE_ANONYMOUS]".equals(userVO.getRole_id()) && "/main".equals(request.getRequestURI())) {
+			return true; // true면 요청한 URL의 controller로 이동
+		}
+		
+		// 요청한 URL이 세션이 없을 때 '/main'으로 이동시키는 조건
+		// - 내정보 변경
+		if ("[ROLE_ANONYMOUS]".equals(userVO.getRole_id()) && "/pro/moveMenu/myPage/mypage_myinfo".equals(request.getRequestURI())) {
+			response.sendRedirect("/main");
+			return false; // 요청한 URL의 controller로 이동 불가
+		}
+		
         /*세션이 없어도 요청한 페이지로 이동*/
         return true;
     }
